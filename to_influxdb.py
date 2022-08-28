46f023d54e32c4a3880209d10a4ff4172478e771
fortune-adekogbe <fortuneadekogbe@gmail.com>
2022-08-29 00:14:01 +0100

    Modified variables

--- to_influxdb.py
+++ to_influxdb.py
@@ -1,6 +1,6 @@
+import os
 import pandas as pd
 import matplotlib.pyplot as plt
-import os
 from influxdb_client import InfluxDBClient, WriteOptions
 from dotenv import load_dotenv
 
@@ -8,27 +8,19 @@ load_dotenv()
 
 df = pd.read_csv("data/jena_climate_2009_2016.csv")[['Date Time', 'T (degC)']]
 df.index = pd.to_datetime(df.pop('Date Time'), format='%d.%m.%Y %H:%M:%S')
-plt.plot(df)
-plt.show()
 df['Measured Fluid'] = ['Air'] * df.shape[0]
-df.to_csv('data/preprocessed.csv')
-print(df.head())
-print(df.shape)
 
-org = "forecasting"
-bucket = "weather_temperature"
-token = os.getenv("INFLUX_TOKEN")
+plt.plot(df['T (degC)'])
+# plt.show()
 
-with InfluxDBClient(url="http://localhost:8086", token=token, org=org) as _client:
+ORG = "forecasting"
+BUCKET = "weather_temperature"
+TOKEN = os.getenv("INFLUX_TOKEN")
 
-    with _client.write_api(write_options=WriteOptions(batch_size=10000,
-                                                      flush_interval=5_000,
-                                                      jitter_interval=1_000,
-                                                      retry_interval=2_000,
-                                                      max_retries=3,
-                                                      max_retry_delay=50_000)) as _write_client:
+with InfluxDBClient(url="http://localhost:8086", token=TOKEN, org=ORG) as _client:
+    with _client.write_api() as _write_client:
         """
         Write Pandas DataFrame
         """
-        _write_client.write(bucket, org, record=df, data_frame_measurement_name='air_temperature',
+        _write_client.write(BUCKET, ORG, record=df, data_frame_measurement_name='air_temperature',
                             data_frame_tag_columns=['Measured Fluid'])
