2eceba95b0f71a3e01dde4a8aab44ac0eedae16a
fortune-adekogbe <fortuneadekogbe@gmail.com>
2022-08-29 00:15:25 +0100

    Added streamlit components.

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-org = "forecasting"
-bucket = "weather_temperature"
+ORG = "forecasting"
+BUCKET = "weather_temperature"
 TOKEN = os.getenv("INFLUX_TOKEN")
 
 
@@ -16,25 +16,38 @@ def get_weather_data(time_range=20):
     """
     Query data and return a Pandas DataFrame
     """
-    client = InfluxDBClient(url="http://localhost:8086", token=TOKEN, org=org)
-
+    client = InfluxDBClient(url="http://localhost:8086", token=TOKEN, org=ORG)
     query_api = client.query_api()
-
-    result_df = query_api.query_data_frame(f'from(bucket:"{bucket}") '
+    result_df = query_api.query_data_frame(f'from(bucket:"{BUCKET}") '
                                            f'|> range(start: -{time_range}y) '
                                            '|> pivot(rowKey:["_time"], columnKey: ["_field"], valueColumn: "_value") '
                                            '|> keep(columns: ["_time","Measured Fluid", "T (degC)"])')
+    result_df['month'] = result_df['_time'].dt.month
+
     client.close()
     return result_df
-
 
 print('Getting started...')
 
 chart_df = get_weather_data()
 
-chart_df.rename(columns={"_time": "Date-Time", "T (degC)": "Temperature (°C)"},
+chart_df.rename(columns={"_time": "Date", "T (degC)": "Temperature (°C)"},
                 inplace=True)
 
-st.title("Air Temperature (°C) Vs. Time")
+st.title("Air Temperature (°C) vs. Time")
 
-chart = st.line_chart(chart_df, x="Date-Time", y="Temperature (°C)")
+st.write("## Line Chart")
+st.line_chart(chart_df, x="Date", y="Temperature (°C)")
+
+st.write("## Area Chart")
+st.area_chart(chart_df, x="Date", y="Temperature (°C)")
+
+chart_month = chart_df.groupby('month')['Temperature (°C)'].mean()
+
+st.write("## Bar Chart\nThis shows the average temperature in each month")
+
+st.bar_chart({'Month number': chart_month.index,
+              'Average Temperature (°C)': chart_month.values},
+             x="Month number",
+             y="Average Temperature (°C)")
+
