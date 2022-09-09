ae47158634049a07ad1115ef6909760eba7a8598
fortune-adekogbe <fortuneadekogbe@gmail.com>
2022-09-10 00:29:16 +0100

    Modified App to depend on local data.

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-import os
-from influxdb_client import InfluxDBClient
+# from influxdb_client import InfluxDBClient
+import pandas as pd
 
 import streamlit as st
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
-load_dotenv()
-
-ORG = "forecasting"
-BUCKET = "weather_temperature"
-TOKEN = os.getenv("INFLUX_TOKEN")
+# load_dotenv()
+#
+# ORG = "forecasting"
+# BUCKET = "weather_temperature"
+# TOKEN = os.getenv("INFLUX_TOKEN")
 
 
 @st.cache
@@ -16,23 +16,27 @@ def get_weather_data(time_range=20):
     """
     Query data and return a Pandas DataFrame
     """
-    client = InfluxDBClient(url="http://localhost:8086", token=TOKEN, org=ORG)
-    query_api = client.query_api()
-    result_df = query_api.query_data_frame(f'from(bucket:"{BUCKET}") '
-                                           f'|> range(start: -{time_range}y) '
-                                           '|> pivot(rowKey:["_time"], columnKey: ["_field"], valueColumn: "_value") '
-                                           '|> keep(columns: ["_time","Measured Fluid", "T (degC)"])')
-    result_df['month'] = result_df['_time'].dt.month
+    # client = InfluxDBClient(url="http://localhost:8086", token=TOKEN, org=ORG)
+    # query_api = client.query_api()
+    # result_df = query_api.query_data_frame(f'from(bucket:"{BUCKET}") '
+    #                                        f'|> range(start: -{time_range}y) '
+    #                                        '|> pivot(rowKey:["_time"], columnKey: ["_field"], valueColumn: "_value") '
+    #                                        '|> keep(columns: ["_time","Measured Fluid", "T (degC)"])')
+    result_df = pd.read_csv('data/preprocessed.csv')
+    result_df['Date Time'] = pd.to_datetime(result_df['Date Time'])
+    result_df['month'] = result_df['Date Time'].dt.month
 
-    client.close()
+    # client.close()
     return result_df
+
 
 print('Getting started...')
 
 chart_df = get_weather_data()
 
-chart_df.rename(columns={"_time": "Date", "T (degC)": "Temperature (°C)"},
+chart_df.rename(columns={"Date Time": "Date", "T (degC)": "Temperature (°C)"},
                 inplace=True)
+print(chart_df.head())
 
 st.title("Air Temperature (°C) vs. Time")
 
